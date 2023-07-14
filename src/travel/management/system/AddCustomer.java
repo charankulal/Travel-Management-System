ebe9316d9effa0990bd19aec549365b55587e897
Charan <charankulal0241@gmail.com>
2023-07-14 13:22:16 +0530

    July 14 1:30 PM
    Adding Add customer functionality

--- src/travel/management/system/AddCustomer.java
+++ src/travel/management/system/AddCustomer.java
@@ -4,78 +4,139 @@ import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 
-public class AddCustomer extends JFrame implements ActionListener{
-    
-    JLabel usernamelbl1,namelbl;
+public class AddCustomer extends JFrame implements ActionListener {
+
+    JLabel usernamelbl1, namelbl;
     JComboBox comboid;
-    JTextField numberfield,addressfield,countryfield;
-    JRadioButton rmale,rfemale;
-    
-    AddCustomer()
-    {
+    JTextField numberfield, addressfield, countryfield, phonefield, emailfield;
+    JRadioButton rmale, rfemale;
+    JButton add, back;
+
+    AddCustomer() {
         setLayout(null);
         getContentPane().setBackground(Color.white);
-        setBounds(450,200,850,550);
-        
-        JLabel usernamelbl=new JLabel("Username");
-        usernamelbl.setBounds(30,50,150,25);
+        setBounds(450, 200, 850, 550);
+
+        JLabel usernamelbl = new JLabel("Username");
+        usernamelbl.setBounds(30, 50, 150, 25);
         add(usernamelbl);
-        
-        usernamelbl1=new JLabel("");
-        usernamelbl1.setBounds(220,50,150,25);
+
+        usernamelbl1 = new JLabel("");
+        usernamelbl1.setBounds(220, 50, 150, 25);
         add(usernamelbl1);
-        
-        JLabel labelid=new JLabel("ID : ");
-        labelid.setBounds(30,90,150,25);
+
+        JLabel labelid = new JLabel("ID : ");
+        labelid.setBounds(30, 90, 150, 25);
         add(labelid);
-        
-        comboid=new JComboBox(new String[] {"Adhaar card","PAN card","Passport","Driving License","Voter Id"});
-        comboid.setBounds(220,90,150,25);
+
+        comboid = new JComboBox(new String[]{"Adhaar card", "PAN card", "Passport", "Driving License", "Voter Id"});
+        comboid.setBounds(220, 90, 150, 25);
         comboid.setBackground(Color.white);
         add(comboid);
-        
-        JLabel numberlbl=new JLabel("Number :");
-        numberlbl.setBounds(30,130,150,25);
+
+        JLabel numberlbl = new JLabel("Number :");
+        numberlbl.setBounds(30, 130, 150, 25);
         add(numberlbl);
-        
-        numberfield=new JTextField();
-        numberfield.setBounds(220,130,150,25);
+
+        numberfield = new JTextField();
+        numberfield.setBounds(220, 130, 150, 25);
         add(numberfield);
-        
-        JLabel namelabel=new JLabel("Name : ");
-        namelabel.setBounds(30,170,150,25);
+
+        JLabel namelabel = new JLabel("Name : ");
+        namelabel.setBounds(30, 170, 150, 25);
         add(namelabel);
-        
-        namelbl=new JLabel("");
-        namelbl.setBounds(220,170,150,25);
+
+        namelbl = new JLabel("");
+        namelbl.setBounds(220, 170, 150, 25);
         add(namelbl);
-        
-        JLabel genderlabel=new JLabel("Gender : ");
-        genderlabel.setBounds(30,210,150,25);
+
+        JLabel genderlabel = new JLabel("Gender : ");
+        genderlabel.setBounds(30, 210, 150, 25);
         add(genderlabel);
-        
-        rmale=new JRadioButton("Male");
+
+        rmale = new JRadioButton("Male");
         rmale.setBackground(Color.white);
-        rmale.setBounds(220,210,75,25);
+        rmale.setBounds(220, 210, 75, 25);
         add(rmale);
-        
-        rfemale=new JRadioButton("Female");
+
+        rfemale = new JRadioButton("Female");
         rfemale.setBackground(Color.white);
-        rfemale.setBounds(300,210,75,25);
+        rfemale.setBounds(300, 210, 75, 25);
         add(rfemale);
-        
-        ButtonGroup gender=new ButtonGroup();
+
+        ButtonGroup gender = new ButtonGroup();
         gender.add(rmale);
         gender.add(rfemale);
+
+        JLabel countrylbl = new JLabel("Country :");
+        countrylbl.setBounds(30, 250, 150, 25);
+        add(countrylbl);
+
+        countryfield = new JTextField();
+        countryfield.setBounds(220, 250, 150, 25);
+        add(countryfield);
+
+        JLabel addresslbl = new JLabel("Address :");
+        addresslbl.setBounds(30, 290, 150, 25);
+        add(addresslbl);
+
+        addressfield = new JTextField();
+        addressfield.setBounds(220, 290, 150, 25);
+        add(addressfield);
+
+        JLabel phonelbl = new JLabel("Phone :");
+        phonelbl.setBounds(30, 330, 150, 25);
+        add(phonelbl);
+
+        phonefield = new JTextField();
+        phonefield.setBounds(220, 330, 150, 25);
+        add(phonefield);
+
+        JLabel emaillbl = new JLabel("Address :");
+        emaillbl.setBounds(30, 370, 150, 25);
+        add(emaillbl);
+
+        emailfield = new JTextField();
+        emailfield.setBounds(220, 370, 150, 25);
+        add(emailfield);
+
+        add = new JButton("Add");
+        add.setBackground(Color.BLACK);
+        add.setForeground(Color.white);
+        add.setBounds(70, 430, 100, 25);
+        add(add);
+
+        back = new JButton("Back");
+        back.setBackground(Color.BLACK);
+        back.setForeground(Color.white);
+        back.setBounds(270, 430, 100, 25);
+        add(back);
+
+        ImageIcon i5 = new ImageIcon(ClassLoader.getSystemResource("icons/newcustomer.jpg"));
+        Image i6 = i5.getImage().getScaledInstance(500, 500, Image.SCALE_DEFAULT);
+        ImageIcon i7 = new ImageIcon(i6);
+        JLabel image1 = new JLabel(i7);
+        image1.setBounds(400, 0, 500, 500);
+        add(image1);
         
+        try{
+            Connectivity c=new Connectivity();
+            c.s.executeQuery("select * from user_account where uername");
+            
+            
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
         setVisible(true);
     }
-    public void actionPerformed(ActionEvent ae)
-    {
-        
+
+    public void actionPerformed(ActionEvent ae) {
+
     }
-    public static void main(String args[])
-    {
+
+    public static void main(String args[]) {
         new AddCustomer();
     }
 }
